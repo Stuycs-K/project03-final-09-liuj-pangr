ca454a03c2365e4a868ab67a6603b278e7fe374d
Jason Liu <jliu50@stuy.edu>
2025-01-07 14:39:26 -0500

    added changes; changing seats

--- rps.c
+++ rps.c
@@ -1,7 +1,22 @@
 #include "rps.h"
 
-char input(){ //When called asks used for input
-
+char takeInput(){ //When called asks used for input
+  while(1){
+    printf("Please enter either 'r'(rock), 'p'(paper), or 's'(scissor)\n");
+    char input[256];
+    fgets(input, 255, stdin);
+    int length = strlen(sentence);
+    if(length > 1){
+      printf("Invalid arguement: too long\n");
+    }
+    char attack = input[0];
+    if(strcmp("r", attack) == 0 || strcmp("p", attack) == 0 || strcmp("s", attack) == 0){
+      return attack;
+    }
+    else{
+      printf("Invalid arguement: not 'r'(rock), 'p'(paper), or 's'(scissor)\n")
+    }
+  }
   return NULL;
 }
 
