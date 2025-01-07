b927715287210be064f2e84042bed1cca1452575
Steven Luo <sluo50@stuy.edu>
2025-01-07 14:50:25 -0500

    Start to client

--- client.c
+++ client.c
@@ -2,5 +2,14 @@
 #include "rps.h"
 
 int main(){
+  int to_server;
+  int from_server = client_handshake( &to_server );
+
+  char choice = takeInput()l
+  int bytes = write(to_server, &choice, sizeof(char));
+  // write to WKP
+
+  //
+
   return 0;
 }
