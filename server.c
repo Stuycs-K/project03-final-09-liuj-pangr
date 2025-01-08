27675bcae8228b4ce651c4739061ee802fd9883b
Renxuan Pang <rpang50@stuy.edu>
2025-01-08 14:52:07 -0500

    class

--- server.c
+++ server.c
@@ -14,30 +14,20 @@
 
 	Think of how merge sort is implemented
  */
-int handleBracket(int addr[][2]){
 
-  return 0;
-}
+struct player { int * downstream; int status; };
+
 int main(){
-  // TODO: Make the server wait until enough clients join
-  // int addrs[TEMP_MAX][2];
-  int adr[2];
-  int * addr = adr;
-  server_setup(addr);
-
-  // for (int i = 0; i < TEMP_MAX; i++) { // assuming that the server needs TEMP_MAX to continue, for testing
-  //   addr = server_handshake(addr);
-  //   addrs[i][0] = adr[0];
-  //   addrs[i][1] = adr[1];
-  // }
-  //
-  // for (int i = 0; i < TEMP_MAX; i++) {
-  //   int check = 300;
-  //   write(addrs[i][0], &check, 4);
-  //   read(addrs[i][1], &check, 4);
-  //   printf("%d\n", check);
-  // }
-  // handleBracket(addr);
+  struct player list[8];
+  char buff[512];
+  int current = 0;
+  while (fgets(buff, 511, stdin)){
+    printf("Looking for clients? input y/n\n");
+    if (strcmp(buff, "y") == 0){
+      list[current] = struct player client;
+    }
+  }
+  // server_setup();
 
   return 0;
 }
