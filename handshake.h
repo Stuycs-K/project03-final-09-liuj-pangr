3287e4762423a95a43dacc0c67d34ad2c7e86189
Steven Luo <stevenl350@nycstudents.net>
2025-01-08 00:41:10 -0500

    Fix define + added LINE_SIZE

--- handshake.h
+++ handshake.h
@@ -8,8 +8,11 @@
 #include <errno.h>
 #include <signal.h>
 
-#ifndef HANDSHAKE_H
 #define WKP "serverPipe"
+#define LINE_SIZE 512
+
+#ifndef HANDSHAKE_H
+#define HANDSHAKE_H
 
 int * server_handshake(int * addr);
 int * client_handshake(int * addr);
